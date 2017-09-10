0b4fa618a2c2dca1d1864a490e13477bef1fc922
John <john_adebiyi@hotmail.de>
2017-09-10 23:24:41 +0200

    Fade in animation
    Fade in animation
    - between main menu scene and option settings scene
    - restarting from checkpoint

---
+++ Scripts/FadeInForGame1Scene.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class FadeInForGame1Scene : MonoBehaviour {
+    public Image blackFadeInOUtImage;//used for fading in
+    public Animator anim;// used for fading in
+}
