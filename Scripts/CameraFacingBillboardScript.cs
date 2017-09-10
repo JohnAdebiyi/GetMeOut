2cbac8cf257e21d0c021a4ec2880d648e4fa07d6
John <john_adebiyi@hotmail.de>
2017-09-10 22:28:07 +0200

    Enemy health bar should always position itself to the player.

---
+++ Scripts/CameraFacingBillboardScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+//Enemy healthbar should always position itself to the fps main camera. 
+//Goes to EnemyCanvas
+public class CameraFacingBillboardScript : MonoBehaviour
+{
+    public Camera fpsCam;
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - fpsCam.transform.position);
+    }
+}
+
+
