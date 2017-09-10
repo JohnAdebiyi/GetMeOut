482a0012acce976aa1f7f663a3a72206ba738fb1
John <john_adebiyi@hotmail.de>
2017-09-10 21:45:47 +0200

    music and sound effects manager
    Makes adding songs or effects much easier.
    A song or soundeffect can be played only using the code
    
    FindObjectOfType<SFX_Manager>().Play("soundeffect");
    FindObjectOfType<MusicManager>().Play("song");

---
+++ Scripts/SoundManager/MusicManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class MusicManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+
+    public static MusicManager instance; // to avoid multiple instances of this gameobject in scenes
+
+	void Awake ()
+    {     
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    
+        DontDestroyOnLoad(gameObject); // if scenes are changed and sound is playing, dont destroy and restart again. Using Prefab MusicManager(GameObject)
+
+        //add an audio source in the inspector for every sound on Awake
+        foreach (Sound s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+           s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    void Start()
+    {
+        Play("intro");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+}
---
+++ Scripts/SoundManager/SFX_Manager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class SFX_Manager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    public static SFX_Manager instance; // to avoid multiple instances of this gameobject in scenes
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject); // if scenes are changed and sound is playing, dont destroy and restart again. Using Prefab SFX_Manager(GameObject)
+
+        //add an audio source in the inspector for every sound on Awake
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+        Debug.Log("played: " + name);
+    }
+
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+        s.source.Stop();
+        Debug.Log("stopped: " + name);
+    }
+}
