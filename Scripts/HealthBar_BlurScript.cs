b6f6b1b30c9c3fffddf019fd87bc46814a54c6c4
John <john_adebiyi@hotmail.de>
2017-09-10 23:13:38 +0200

    Players health bar border blurs red for 0.10 seconds when damage is taken

---
+++ Scripts/HealthBar_BlurScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar_BlurScript : MonoBehaviour {
+
+    public Image barRedBlur;
+
+    // Use this for initialization
+    void Start ()
+    {
+        barRedBlur.enabled = false;  
+    }
+
+
+    public void ShowBlur()
+    {
+        StartCoroutine(OnOff_Healthblur());
+    }
+
+    IEnumerator OnOff_Healthblur()
+    {
+        barRedBlur.enabled = true;
+        yield return new WaitForSeconds(0.10f);
+        barRedBlur.enabled = false;
+    }
+}
