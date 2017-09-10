a80be6837d0fe254c7da612d4ec251245e2093bb
John <john_adebiyi@hotmail.de>
2017-09-10 22:25:24 +0200

    when enemy dies, an items is left at its place
    4 enemies leave a health cross behind on death
    1 enemy leaves a paper items behind which depicts the password for the save in the bedroom

---
+++ Scripts/EnemyHealthScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class EnemyHealthScript : MonoBehaviour {
+
+    public float maxHealth = 15f; // health of the enemy
+    private float currentHealth = 0f;
+    private static int enemiesDead = 0;
+
+    private static int enemiesDeadCounter = 5;
+    public Text enemiesCounterText;
+    public GameObject enemiesCounterPanel;
+
+    public Image bar;
+    public GameObject destroyedVersion; //using Prefab MonsterNormalAnimFall (Animation) or MonsterNormalNoAnimFall Rigidbody(physics)
+    public GameObject displayItem;// for displaying the cross health or paper
+
+    public GameObject crossHair_InRangeGreen;// for deactivating the cross hair when enemy dies
+    public GameObject crossHair_InRangeBlack;// for deactivating the cross hair when enemy dies
+
+    public GameObject pointerToEnemy_Red;//for destroying the pointer
+    public GameObject pointerToEnemy_Yellow;//for destroying the pointer
+
+    public GameObject pointerToKeycardTerminal;
+
+    void Start()
+    {
+        //Image bar - full bar at start
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar();
+        if (currentHealth <= 0f)
+        {
+            Die();
+            crossHair_InRangeBlack.SetActive(false);
+            crossHair_InRangeGreen.SetActive(false);
+            FindObjectOfType<SFX_Manager>().Play("stoneDrop");
+        }
+    }
+
+    //Image bar
+    void healthBar()
+    {
+        bar.fillAmount = currentHealth / maxHealth; // procent value from 0 - 1. example -  60/100 = 0,6% on the bar      
+    }
+
+
+    void Die()
+    {
+
+        
+        Instantiate(displayItem, transform.position, transform.rotation);// display cross health or paper
+        Instantiate(destroyedVersion, transform.position, transform.rotation);//display the enemy falling to the ground
+        Destroy(gameObject);// destroy the enemy
+        pointerToEnemy_Red.SetActive(false);
+        pointerToEnemy_Yellow.SetActive(false);
+        pointerToEnemy_Red = null;
+        pointerToEnemy_Yellow = null;
+
+        enemiesDead += 1;
+        Debug.Log("enemies dead: " + enemiesDead);
+        //if all enemies are dead, open the rock doors
+        if (enemiesDead == 5)
+        {
+            Debug.Log("dead");
+            Destroy(GameObject.FindWithTag("caveDoor1"));// destroy paper item
+            Destroy(GameObject.FindWithTag("caveDoor2"));// destroy paper item
+
+            enemiesCounterPanel.SetActive(false);
+            pointerToKeycardTerminal.SetActive(true);
+        }
+
+
+        enemiesDeadCounter -= 1;
+        enemiesCounterText.text = "Enemies: " + enemiesDeadCounter.ToString();//health text
+    }
+
+
+}
---
+++ Scripts/Take_OutsideItemsScript.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class Take_OutsideItemsScript : MonoBehaviour
+{
+
+    public GameObject openPanel = null;
+    public GameObject showpassword_Panel;
+    public Camera fpsCam;
+    private PlayerStatusScript giveHealth;
+
+
+    public string openText = "Take item";
+    public string closeText = "";
+    private bool _isOpen = false;
+
+    private bool inTrigger;
+    private bool inTrigger1;
+    private bool inTrigger2;
+    private bool inTrigger3;
+    private bool inTrigger4;
+
+    private float health = 0.10f;
+
+
+    void Start()
+    {
+        giveHealth = GetComponent<PlayerStatusScript>();
+    }
+
+    // for checking if the health cross panel is activ
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return openPanel.activeInHierarchy;
+        }
+    }
+
+    // for updating the health cross panel text
+    private void UpdatePanelText()
+    {
+        UnityEngine.UI.Text panelText = openPanel.transform.FindChild("Text").GetComponent<UnityEngine.UI.Text>();
+        if (panelText != null)
+        {
+            panelText.text = _isOpen ? closeText : openText;//if _isOpen is true return CloseText or else return openText
+        }
+    }
+
+
+    // if raycast hits the health cross collider, set _isInsideTrigger to true else set to false
+    void _RaycastHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 2))
+        {
+            //health cross 0
+            if (hit.collider.gameObject.tag == "take_Health")
+            {
+                inTrigger = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+            //health cross 1
+            if (hit.collider.gameObject.tag == "take_Health(1)")
+            {
+                inTrigger1 = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+            //health cross 2
+            if (hit.collider.gameObject.tag == "take_Health(2)")
+            {
+                inTrigger2 = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+            //health cross 3
+            if (hit.collider.gameObject.tag == "take_Health(3)")
+            {
+                inTrigger3 = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+            //health cross 4
+            if (hit.collider.gameObject.tag == "take_PaperOutside")
+            {
+                inTrigger4 = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+
+
+        }
+        else
+        {
+            openPanel.SetActive(false);
+            inTrigger = false;
+            inTrigger1 = false;
+            inTrigger2 = false;
+            inTrigger3 = false;
+            inTrigger4 = false;
+        }
+    }
+
+
+
+    // if _isInsideTrigger is true and mouse is pressed destroy object and increase health 
+    void InsideTrigger()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject()) //stop raycast on UI clicks. when UI is activ, gameObjects arent hit with raycast.
+        {
+            //health cross 0
+            if (inTrigger)
+            {
+
+                if (Input.GetMouseButtonDown(1))
+                {
+                    giveHealth.IncreaseHealth(health);// increase health
+                    Destroy(GameObject.FindWithTag("take_Health"));// destroy cross health item
+                    openPanel.SetActive(false);// panel is invincible
+                    inTrigger = false;
+                    FindObjectOfType<SFX_Manager>().Play("gotItem");
+                }
+            }
+            //health cross 1
+            if (inTrigger1)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    giveHealth.IncreaseHealth(health);// increase health
+                    Destroy(GameObject.FindWithTag("take_Health(1)"));// destroy cross health item
+                    openPanel.SetActive(false);// panel is invincible
+                    inTrigger1 = false;
+                    FindObjectOfType<SFX_Manager>().Play("gotItem");
+                }
+            }
+            //health cross 2
+            if (inTrigger2)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    giveHealth.IncreaseHealth(health);// increase health
+                    Destroy(GameObject.FindWithTag("take_Health(2)"));// destroy cross health item
+                    openPanel.SetActive(false);// panel is invincible
+                    inTrigger2 = false;
+                    FindObjectOfType<SFX_Manager>().Play("gotItem");
+                }
+            }
+            //health cross 3
+            if (inTrigger3)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    giveHealth.IncreaseHealth(health);// increase health
+                    Destroy(GameObject.FindWithTag("take_Health(3)"));// destroy cross health item
+                    openPanel.SetActive(false);// panel is invincible
+                    inTrigger3 = false;
+                    FindObjectOfType<SFX_Manager>().Play("gotItem");
+                }
+            }
+            //paper
+            if (inTrigger4)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    showpassword_Panel.SetActive(true);
+                    Destroy(GameObject.FindWithTag("take_PaperOutside"));// destroy paper item
+                    openPanel.SetActive(false);// panel is invincible
+                    inTrigger4 = false;
+                    FindObjectOfType<SFX_Manager>().Play("gotItem");
+                }
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        _RaycastHit();
+        InsideTrigger();
+    }
+}
