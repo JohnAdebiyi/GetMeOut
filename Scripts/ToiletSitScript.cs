4085ee80dd8dcb21b507224c4ed86c107e9d2476
John <john_adebiyi@hotmail.de>
2017-09-10 18:47:21 +0200

    Toilet lid
    if player is in front of the toilet lid, right mouse can be clicked to open the lid, so keycard can be taken.

---
+++ Scripts/ToiletSitScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class ToiletSitScript : MonoBehaviour
+{
+
+
+    public Animator _animator;
+    public GameObject openPanel = null;
+    public Collider toiletSitCollider;
+    public GameObject keycard_Toilet;
+    public Camera fpsCam;
+
+    private bool _isInsideTrigger = false;
+
+    public string openText = "Open toilet seat";
+    public string closeText = "";
+
+    private bool _isOpen = false;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+    // for checking if the toilet lid panel is activ
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return openPanel.activeInHierarchy;
+        }
+    }
+
+    // for updating the toilet lid panel text
+    private void UpdatePanelText()
+    {
+        UnityEngine.UI.Text panelText = openPanel.transform.FindChild("Text").GetComponent<UnityEngine.UI.Text>();
+        if (panelText != null)
+        {
+            panelText.text = _isOpen ? closeText : openText;//if _isOpen is true return CloseText or else return openText
+        }
+    }
+
+
+    // if raycast hits the toiletsit collider, set _isInsideTrigger to true else set to false
+    void _RaycastHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 3))
+        {
+            if (hit.collider.gameObject.tag == "ToiletSit")
+            {
+                _isInsideTrigger = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+        }
+        else
+        {
+            _isInsideTrigger = false;
+            openPanel.SetActive(false);
+        }
+
+    }
+
+    //if _isInsideTrigger is true and mouse is pressed open toilet lid
+    void InsideTrigger()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject()) //stop raycast on UI clicks. when UI is activ, gameObjects arent hit with raycast.
+        { 
+                if (IsOpenPanelActive && _isInsideTrigger)
+                {
+                    if (Input.GetMouseButtonDown(1))
+                    {
+                        _animator.SetBool("open_ToiletSeat", true);
+                        keycard_Toilet.SetActive(true);
+                        toiletSitCollider.enabled = false;
+                        FindObjectOfType<SFX_Manager>().Play("toiletSeatUp");
+                    }
+                }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _RaycastHit();
+        InsideTrigger();
+    }
+}
+
