ff3b709d38c0aaa15c0d0d9a90657a9dd01d055d
John <john_adebiyi@hotmail.de>
2017-09-10 23:48:06 +0200

    open book
    When player looks at the open book on the table, a larger book appears in the middle of the screen with a depiction of how to count in binary.

---
+++ Scripts/Book_BinaryScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Book_BinaryScript : MonoBehaviour {
+
+  //public Image bookBinary;
+    public GameObject bookBinary;
+    public Camera fpsCam;
+
+    void Start()
+    {
+        //bookBinary.enabled = false;
+    }
+
+    // if raycast hits the open book collider
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 2))
+        {
+            if (hit.collider.gameObject.tag == "Book_binary")
+            {
+                //bookBinary.enabled = true;
+                bookBinary.SetActive(true);
+            }
+            if (hit.collider.gameObject.tag == "Game_Laptop")// in case raycast hits the laptop then deactivate the laptop panel
+            {
+                //bookBinary.enabled = false;
+                bookBinary.SetActive(false);
+            }
+        }
+        else
+        {
+            //bookBinary.enabled=false;
+            bookBinary.SetActive(false);
+        }
+    }
+}
