818e36281c5eb8544a19ec4db1e9d9ccbd3f86d6
John <john_adebiyi@hotmail.de>
2017-09-10 22:32:28 +0200

    Shielded Enemy
    When a specific enemy is shot, the enemy´s shield gets activated.
    
    Player has to first destroy the shield to inflict damage on the enemy.

---
+++ Scripts/Shielded_EnemyScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Shielded_EnemyScript : MonoBehaviour {
+
+    public GameObject enemy;                     // when shield is destroyed change the tag(shielded_enemy) of monster(4) to tag(enemy), 
+                                                 //so that if(hit.collider.tag == "enemy") can be executed in WeaponGunScript.cs
+
+    public ParticleSystem shieldEffectSharpRing;
+    public ParticleSystem shieldEffectParticles;
+    public ParticleSystem shieldEffectGlowingRing;
+
+
+
+    public float maxHealthShield = 30f;
+    private float currentHealthShield = 0f;
+    public Image barShield;
+
+    void Start()
+    {
+        //Image bar - full bar at start
+        currentHealthShield = maxHealthShield;
+    }
+
+    public void shield()
+    {
+        shieldEffectSharpRing.Play();
+        shieldEffectParticles.Play();
+        shieldEffectGlowingRing.Play();
+        FindObjectOfType<SFX_Manager>().Play("shield");
+    }
+
+
+
+    public void TakeDamageShield(float damageShield)
+    {
+        currentHealthShield -= damageShield;
+        healthBarShield();
+        if (currentHealthShield <= 0f)
+        {
+            enemy.tag = "enemy";// execute WeaponGunscripts.cs -> if(hit.collider.tag == "enemy")
+        }
+    }
+
+    //Image bar
+    void healthBarShield()
+    {
+        barShield.fillAmount = currentHealthShield / maxHealthShield; // procent value from 0 - 1. example -  60/100 = 0,6% on the bar      
+    }
+}
