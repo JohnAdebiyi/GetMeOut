d8ae0f78f1470874c121472e7133d219c30d5365
John <john_adebiyi@hotmail.de>
2017-09-10 23:06:35 +0200

    Bedroom and livingroom safes
    If player is in front of the bedroom or livingroom safe, a keypad panel appears.
    
    Safe can only be opened when correct password is typed in.

---
+++ Scripts/Keypad_BedRoom_SAFE_Script.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Keypad_BedRoom_SAFE_Script : MonoBehaviour
+{
+
+    public GameObject openPanel_Keypad = null;
+    public Animator _animator;
+    public GameObject destroyPasswordMinimalized;//using tag to destroy
+    public Camera fpsCam;
+    public Collider keycard_Terminal1;// activate the keycard collider when door is opened
+    public GameObject test;
+
+    public string currentPassword_BedRoom;
+    public bool bedroom_inTrigger;
+    public bool bedroom_doorOpended;
+    public bool bedroom_keyPadScreen;
+    //public Transform DoorHolder;
+
+
+
+    //keypad input
+    string bedroom_number = null;
+    public InputField bedroom_myNumber = null;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+     
+    }
+
+
+    void Update()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 2))
+        {
+            if (hit.collider.gameObject.tag == "safe_Bedroom")
+            {
+                bedroom_inTrigger = true;
+                openPanel_Keypad.SetActive(true);
+            }
+            if (hit.collider.gameObject.tag == "chestdrawer_Bedroom")// in case raycast hits the chestdrawer Safe then deactivate the bedroom safe panel
+            {
+                openPanel_Keypad.SetActive(false);
+            }
+
+        }
+        else
+        {
+            bedroom_inTrigger = false;
+            bedroom_keyPadScreen = false;
+            openPanel_Keypad.SetActive(false);
+
+        }
+
+
+        if (!EventSystem.current.IsPointerOverGameObject()) //stop raycast on UI clicks. when UI is activ, gameObjects arent hit with raycast.
+        {
+            //if inside the trigger 
+            if (bedroom_inTrigger)
+            {
+
+                //keyboard
+                if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+                {
+                    keypadInput("0");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+                {
+                    keypadInput("1");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+                {
+                    keypadInput("2");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+                {
+                    keypadInput("3");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+                {
+                    keypadInput("4");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
+                {
+                    keypadInput("5");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
+                {
+                    keypadInput("6");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    keypadInput("7");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+                {
+                    keypadInput("8");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    keypadInput("9");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+                if (Input.GetKeyDown(KeyCode.Hash))//hash key not working. Maybe a bug in unity 5.4.0
+                {
+                    keypadInput("#");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+                if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.KeypadMultiply))
+                {
+                    keypadInput("*");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+                if (Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Backspace))
+                {
+
+                    bedroom_myNumber.text = "";
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+
+                }
+
+            }
+
+
+        }
+
+
+
+        if (bedroom_doorOpended)
+        {           
+            _animator.SetBool("Safe_DoorOpen_Bedroom", true);
+            openPanel_Keypad.SetActive(false);
+            bedroom_inTrigger = false;
+            bedroom_keyPadScreen = false;
+            Destroy(destroyPasswordMinimalized);
+            keycard_Terminal1.enabled = true;//collider is enabled       
+        }
+    }
+
+
+
+    //Number is passed from on Click() function when clicked with mouse 
+    public void keypadInput(string bedroom_key)
+    {
+
+        bedroom_number = bedroom_key;
+        bedroom_myNumber.text += bedroom_number;
+        
+        if(bedroom_key == "d")
+        {
+            bedroom_myNumber.text = "";
+        }
+        if (bedroom_myNumber.text == currentPassword_BedRoom)
+        {
+            FindObjectOfType<SFX_Manager>().Play("openSafe");
+            bedroom_doorOpended = true;
+        }
+
+    }
+}
+
---
+++ Scripts/Keypad_LivingRoom_SAFE_Script.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Keypad_LivingRoom_SAFE_Script : MonoBehaviour
+{
+
+    public GameObject openPanel_Keypad;
+    public Animator _animator;
+    public GameObject destroyPasswordMinimalized;//using tag to destroy
+    public Collider keycard;
+
+    public Camera fpsCam;
+    public string currentPassword_LivingRoom;
+    public bool inTrigger;
+    public bool doorOpended;
+    public bool keyPadScreen;
+
+    //keypad input
+    string number = null;
+    public InputField myNumber = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 2))
+        {
+            if (hit.collider.gameObject.tag == "Safe_Livingroom")
+            {
+                inTrigger = true;
+                openPanel_Keypad.SetActive(true);// panel can now being seen                
+            }
+            
+        }
+        else
+        {
+            inTrigger = false;
+            keyPadScreen = false;
+            openPanel_Keypad.SetActive(false);// panel can now being seen          
+        }
+
+
+
+        destroyPasswordMinimalized = GameObject.FindGameObjectWithTag("Panel_minimized");
+
+        if (!EventSystem.current.IsPointerOverGameObject()) //stop raycast on UI clicks. when UI is activ, gameObjects arent hit with raycast.
+        {
+            //if inside the trigger 
+            if (inTrigger)
+            {
+
+
+
+                //keyboard
+                if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+                {
+                    keypadInput("0");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+                {
+                    keypadInput("1");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+                {
+                    keypadInput("2");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+                {
+                    keypadInput("3");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+                {
+                    keypadInput("4");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
+                {
+                    keypadInput("5");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
+                {
+                    keypadInput("6");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    keypadInput("7");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+                {
+                    keypadInput("8");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    keypadInput("9");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+                if (Input.GetKeyDown(KeyCode.Hash))//hash key not working. Maybe a bug in unity 5.4.0
+                {
+                    keypadInput("#");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+                if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.KeypadMultiply))
+                {
+                    keypadInput("*");
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+                }
+                if (Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Backspace))
+                {
+
+                    myNumber.text = "";
+                    FindObjectOfType<SFX_Manager>().Play("clickButtonKeypad");
+
+                }
+
+            }
+        }
+      
+  
+        if (doorOpended)
+        {
+            _animator.SetBool("Safe_DoorOpen_Livingroom", true);
+            openPanel_Keypad.SetActive(false);
+            inTrigger = false;
+            keyPadScreen = false;
+            Destroy(destroyPasswordMinimalized);
+            keycard.enabled = true;            
+        }
+
+    }
+
+    //Number is passed from mouse on Click() function when clicked with mouse 
+    public void keypadInput(string key)
+    {
+        
+        number = key;
+        myNumber.text += number;
+        
+        if(key == "d")
+        {
+            myNumber.text = "";
+        }
+        if (myNumber.text == currentPassword_LivingRoom)
+        {
+            FindObjectOfType<SFX_Manager>().Play("openSafe");
+            doorOpended = true;
+        }
+    }
+}
+
