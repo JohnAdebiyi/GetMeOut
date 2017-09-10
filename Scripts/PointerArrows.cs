b406966e2bf54866ae0227b5b068fe0eb4bb6991
John <john_adebiyi@hotmail.de>
2017-09-10 21:21:58 +0200

    Pointers pointing(guiding) to enemies, items or mission objective

---
+++ Scripts/PointerArrows.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+public class PointerArrows : MonoBehaviour {
+
+
+
+    public Transform target;
+
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - target.transform.position);
+    }
+}
