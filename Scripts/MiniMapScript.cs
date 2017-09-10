90927d7bcc40a935732bcb656ca573a8fd8ca079
John <john_adebiyi@hotmail.de>
2017-09-10 21:34:47 +0200

    Minimap
    Minimap showing the position of the player, enemy and items.
    
    If player enter the enemy´s eye sight, the enemy´s symbol on the minimap turns red.
    
    If player out runs the enemy, it goes back to yellow

---
+++ Scripts/MiniMapScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class MiniMapScript : MonoBehaviour {
+
+    public Transform player;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y + 20, player.position.z);
+	}
+}
---
+++ Scripts/MiniMap_ChangeIconColor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+public class MiniMap_ChangeIconColor : MonoBehaviour {
+
+    public bool changeMiniMapColor = false;
+    public Renderer rend;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+         if (changeMiniMapColor == true)
+        {
+            rend.material.color = Color.red;
+        }
+        else
+        {
+            rend.material.color = Color.yellow;
+        }
+	}
+}
