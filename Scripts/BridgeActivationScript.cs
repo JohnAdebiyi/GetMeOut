15e83bb3f968b1184dbc34d21e9f088f3f2b1f5e
John <john_adebiyi@hotmail.de>
2017-09-10 22:53:28 +0200

    Terminal activation
    if key card is inserted, the terminal gets activated and a holographical interface appears with the message that a brigde has appeared, which the player should cross.
    
    A pointer appears directing the player to the bridge.

---
+++ Scripts/BridgeActivationScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+public class BridgeActivationScript : MonoBehaviour {
+
+    public GameObject bridge;// activate the bridge
+    public GameObject bridgeDoor1;// deactivate the invincible bridge door
+    public GameObject bridgeDoor2;// deactivate the invincible bridge door
+    public GameObject bridgeDoor3;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_4;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_5;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_6;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_7;// deactivate the invincible bridge door
+
+    public GameObject pointerToBridge;
+    public GameObject pointerToCave;
+
+    public static bool bridgeIsActiv = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (bridgeIsActiv)
+        {
+            bridge.SetActive(true);
+            bridgeDoor1.SetActive(false);
+            bridgeDoor2.SetActive(false);
+            bridgeDoor3.SetActive(false);
+            bridgeDoor_4.SetActive(false);
+            bridgeDoor_5.SetActive(false);
+            bridgeDoor_6.SetActive(false);
+            bridgeDoor_7.SetActive(false);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        pointerToBridge.SetActive(false);
+        pointerToCave.SetActive(true);
+    }
+    
+
+
+}
---
+++ Scripts/Terminal_1Script.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class Terminal_1Script : MonoBehaviour {
+
+    public Animator _animator;
+    public GameObject openPanel = null;
+    public GameObject destroyKeycardMinimalized005;// important! using Tag to destroy // for destroying the panel keycard obtainded
+    public Camera fpsCam;
+    public GameObject interf;//interface effect
+    public GameObject pointerToBridge;
+    public GameObject pointerToTerminal;
+
+    public bool inTrigger;
+    public string openText = "Insert keycard to activate Terminal";
+    public string closeText = "";
+    private bool _isOpen = false;
+    public static bool keyCardTerminal1 = false;// if player has a card set to true
+    private bool interfaceHasAlreadyBeenActivated = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();        
+    }
+
+
+    // for checking if the terminal panel is activ
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return openPanel.activeInHierarchy;
+        }
+    }
+
+    // for updating the terminal panel text 
+    private void UpdatePanelText()
+    {
+        UnityEngine.UI.Text panelText = openPanel.transform.FindChild("Text").GetComponent<UnityEngine.UI.Text>();
+        if (panelText != null)
+        {
+            panelText.text = _isOpen ? closeText : openText;//if _isOpen is true return closeText or else return openText
+        }
+    }
+
+
+    // if raycast hits the terminal collider, set _isInsideTrigger to true else set to false
+    void _RaycastHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 2))
+        {
+            if (hit.collider.gameObject.tag == "slot_Terminal1" || hit.collider.gameObject.tag == "screen_Terminal1")
+            {
+                _animator.SetBool("Terminal1Slot_Error", true);
+                inTrigger = true;
+                UpdatePanelText();
+                openPanel.SetActive(true);
+            }
+        }
+        else
+        {
+            _animator.SetBool("Terminal1Slot_Error", false);
+            inTrigger = false;
+            openPanel.SetActive(false);
+        }
+
+    }
+    //if _isInsideTrigger is true, key card is obtainded and mouse is pressed
+    void InsideTrigger()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject()) //stop raycast on UI clicks. when UI is activ, gameObjects arent hit with raycast.
+        {
+            if (inTrigger == true)
+            {
+                if (keyCardTerminal1)
+                {
+                    if (Input.GetMouseButtonDown(1))
+                    {
+                        FindObjectOfType<SFX_Manager>().Play("openInterface");
+                        _animator.SetBool("Terminal1Slot_NoError", true);
+                        _animator.SetBool("Terminal_On", true);
+                        openPanel.SetActive(false);
+                        openPanel = null;
+                        interf.SetActive(true);
+                        interfaceHasAlreadyBeenActivated = true;
+                        BridgeActivationScript.bridgeIsActiv = true;
+                        pointerToBridge.SetActive(true);
+                        pointerToTerminal.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    // destroy the mini panel "keycard obtainded" when terminal is activ
+    void DestroyPanel()
+    {
+        destroyKeycardMinimalized005 = GameObject.FindGameObjectWithTag("keycard005");
+        if (_animator.GetBool("Terminal_On") == true)
+        {
+            Destroy(destroyKeycardMinimalized005);            
+        }
+    }
+
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (interfaceHasAlreadyBeenActivated)
+        {
+            if (other.tag == "Player")
+            {          
+                interf.SetActive(false);
+                FindObjectOfType<SFX_Manager>().Play("closeInterface");
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (interfaceHasAlreadyBeenActivated)
+        {
+            if (other.tag == "Player")
+            {       
+                interf.SetActive(true);
+                FindObjectOfType<SFX_Manager>().Play("openInterface");
+            }
+        }
+    }
+
+   // Update is called once per frame
+   void Update()
+    {
+        _RaycastHit();
+        InsideTrigger();
+        DestroyPanel();
+    }
+
+}
