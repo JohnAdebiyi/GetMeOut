49f5a892ed0a7c05ade035e37ff0400e4a4705fc
John <john_adebiyi@hotmail.de>
2017-09-10 21:42:25 +0200

    Disabling and enabling image effect - color correction curve script
    if player leaves the house, color correction curve gets disabled and is enabled if player enters the house

---
+++ Scripts/DeactivateColorCorrectionCurve.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+public class DeactivateColorCorrectionCurve : MonoBehaviour {
+
+    public Camera fpsCam;// for putting off color correction curve when player leaves the house
+
+
+    //using couch wall.fbx as box collider
+    void OnTriggerEnter(Collider other)
+    {
+        fpsCam.GetComponent<ColorCorrectionCurves>().enabled = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        fpsCam.GetComponent<ColorCorrectionCurves>().enabled = false;
+    }
+}
