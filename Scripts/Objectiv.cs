57035d52d806bf9010b18fa3b1e8accace8e9b96
John <john_adebiyi@hotmail.de>
2017-09-10 21:29:08 +0200

    Objective when game starts
    When game starts a panel appears covering the whole screen with the text that the player has 5 minutes to collect 4 keycards. On fail player dies.
    
    As long as the panel is opened, the player is stopped from moving and timescale is set to 0, stopping all movements around the players enviroment.
    
    Only the mouse cursor movement is enabled.
    
    If player closes the panel, all movements are enabled.

---
+++ Scripts/Objectiv.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Objectiv : MonoBehaviour {
+
+    public Image objectiv;
+    public Image objectivBackground;
+    public Button closeObjectiv;
+    public Transform Player;
+
+    void Start ()
+    {
+
+        objectivBackground.gameObject.SetActive(true);
+        objectiv.gameObject.SetActive(true);
+        Time.timeScale = 0; //stop every movement around the enviroment
+        Player.GetComponent<FirstPersonController>().enabled = false; //stop the player from moving
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true; // show cursor
+        IsPause.escape_buttonsEnabled = false; // disable the pause button
+    }
+
+    public void CloseObjektiv()
+    {
+        FindObjectOfType<SFX_Manager>().Play("buttonSound");
+
+        objectivBackground.gameObject.SetActive(false);
+        objectiv.gameObject.SetActive(false);
+
+        Time.timeScale = 1;     //allow movements around the enviroment
+        Player.GetComponent<FirstPersonController>().enabled = true;// allow player movement
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        IsPause.escape_buttonsEnabled = true; // enable the pause button
+    }
+}
